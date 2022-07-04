cdc9f980527eb2748d252002918f27fcacc8120a
hc <894392782@qq.com>
2022-07-04 21:19:46 +0800

    swagger配置

--- ggkt_parent/service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/TeacherController.java
+++ ggkt_parent/service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/TeacherController.java
@@ -3,11 +3,10 @@ package com.atguigu.ggkt.vod.controller;
 
 import com.atguigu.ggkt.model.vod.Teacher;
 import com.atguigu.ggkt.vod.service.TeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,18 +18,28 @@ import java.util.List;
  * @author hc
  * @since 2022-07-04
  */
+@Api(tags = "讲师管理接口")
 @RestController
 @RequestMapping("/admin/vod/teacher")
 public class TeacherController {
+
     @Autowired
     private TeacherService teacherService;
     //http://localhost:8301/admin/vod/teacher/findAll
     //查询所有讲师
     @GetMapping("findAll")
+    @ApiOperation("查询所有讲师")
     public List<Teacher> findAllTeacher(){
         //调用service方法
         List<Teacher> list = teacherService.list();
         return list;
+    }
+    //逻辑删除讲师
+    @DeleteMapping("remove/{id}")
+    @ApiOperation("逻辑删除讲师")
+    public boolean removeTeacher(@PathVariable Long id){
+        boolean isSuccess = teacherService.removeById(id);
+        return isSuccess;
     }
 }
 
