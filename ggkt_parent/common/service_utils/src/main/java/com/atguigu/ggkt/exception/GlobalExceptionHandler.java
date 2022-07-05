9df671303983629ff2592de81b7c838aff59da8c
hc <894392782@qq.com>
2022-07-05 15:01:04 +0800

    自定义异常处理处理实现

--- ggkt_parent/common/service_utils/src/main/java/com/atguigu/ggkt/exception/GlobalExceptionHandler.java
+++ ggkt_parent/common/service_utils/src/main/java/com/atguigu/ggkt/exception/GlobalExceptionHandler.java
@@ -15,4 +15,20 @@ public class GlobalExceptionHandler {
         e.printStackTrace();
         return Result.fail(null).message("执行全局异常处理");
     }
+
+    //特定异常处理ArithmeticException。优先执行特定异常处理
+    @ExceptionHandler(ArithmeticException.class)
+    @ResponseBody
+    public Result error(ArithmeticException e){
+        e.printStackTrace();
+        return Result.fail(null).message("执行全局异常处理");
+    }
+
+    //自定义异常处理
+    @ExceptionHandler(GgktException.class)
+    @ResponseBody
+    public Result error(GgktException e){
+        e.printStackTrace();
+        return Result.fail(null).code(e.getCode()).message(e.getMsg());
+    }
 }
