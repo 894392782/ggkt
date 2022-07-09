1be30f4b7387fcd2b9de3318956489fe400279d9
hc <894392782@qq.com>
2022-07-09 14:35:56 +0800

    营销管理实现

---
+++ ggkt_parent/service/service_activity/src/main/java/com/atguigu/ggkt/activity/service/CouponUseService.java
@@ -0,0 +1,16 @@
+package com.atguigu.ggkt.activity.service;
+
+import com.atguigu.ggkt.model.activity.CouponUse;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 优惠券领用表 服务类
+ * </p>
+ *
+ * @author hc
+ * @since 2022-07-09
+ */
+public interface CouponUseService extends IService<CouponUse> {
+
+}
